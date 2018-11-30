b61ea22d65821b9fa439718b5e59f2bd3a03969c
Chagovskaya Tanya <raze8882@gmail.com>
2018-11-30 22:16:33 +0200

    refactor gitignor fies & remove files

---
+++ galfrost.github.io/dist/js/page-68.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<?php
+    get_header();
+?>
+
+
+<?php
+/* Start the Loop */
+
+//if(have_posts()) : while(have_posts()) : the_post();
+   get_template_part('inc/product', 'growing', get_post_format());
+//endwhile;
+//endif;
+?>
+<?php
+    get_footer();
+?>
